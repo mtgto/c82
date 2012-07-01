830573147f9f2b708bfc5c37a27944c601f64d4e
mtgto <hogerappa@gmail.com>
2012-07-01 18:36:52 +0900

    add tex source.

---
+++ SOCKET_HOST/SOCKET_HOST/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SOCKET_HOST
+//
+//  Created by mtgto on 6/17/12.
+//  Copyright (c) 2012 mtgto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SOCKET_HOST/SOCKET_HOST/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  SOCKET_HOST
+//
+//  Created by mtgto on 6/17/12.
+//  Copyright (c) 2012 mtgto. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
